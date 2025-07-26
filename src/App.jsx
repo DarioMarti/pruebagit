f5533b068ae517091ab7f5280b607deed781c2ae
Dario <lpz.dariomarti@gmail.com>
2025-07-26 17:22:12 +0200

    Primer commit de prueba

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+    <header className="w-full font-black text-white text-2xl shadow-xl bg-amber-600 h-36 flex justify-center items-center">
+      <h1>¡HELLO GIT!</h1>
+    </header>
+    </>
+  )
+}
+
+export default App
